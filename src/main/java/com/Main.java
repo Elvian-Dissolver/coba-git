35b08a5a2e6941e19772266e2d3ce82300d28bc9
indraEl <indra.elvian@gmail.com>
2018-10-04 14:47:39 +0700

    add hello from another world

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
         System.out.print("Hello");
+        System.out.print("Hello from another world");
     }
 }
